352602959b0e7d81ccdee582ee1d3609b1c1abe0
smfahiim25 <smfahimhossen@gmail.com>
2022-03-01 10:54:11 +0600

    code added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-console.log('connect')
+const searchPhone = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    // console.log(searchText);
+    searchField.value = '';
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySearchResult(data.data))
+}
+const displaySearchResult = phones => {
+    const searchResult = document.getElementById('search-result');
+    phones.forEach(phone => {
+        console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = ` 
+        <div class="card h-100">
+            <img class="container w-75" src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${phone.phone_name}</h5>
+            <p class="card-text">${phone.brand}</p>
+            </div>
+        </div>
+        `;
+        searchResult.appendChild(div);
+    })
+}
